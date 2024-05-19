5bb27fbc832d36a2b4eefdb2379830383de1b2a0
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-05-19 22:32:34 +0300

    A markup has been created. A custom control “Thimbles” from the custom element library is connected.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SVPP_CS_WPF_Lab1_task1_individual__Thimbles
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
