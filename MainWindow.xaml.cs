d0a22ebebab0bc56452e7537b4b61036aa54a778
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-05-20 02:48:35 +0300

    Added event handlers. Changed the appearance of elements.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SVPP_CS_WPF_Lab1_task1_individual__Thimbles
 {
@@ -16,11 +17,60 @@ namespace SVPP_CS_WPF_Lab1_task1_individual__Thimbles
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        public int numberAttempts = 1;
+        private int gamesPlayed = 0;
+        private Dictionary<string, int> speedVariants = new() 
+        { {"1x", 3000 }, { "2x", 2000 }, { "3x", 1000 } };
+        private int speed = 3000;
+
         public MainWindow()
         {
             InitializeComponent();
             
+            Binding b = new Binding();
+            b.RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, this.GetType(), 1);
+            b.Path = new PropertyPath(nameof(numberAttempts));
+            TextBlock_Attempts.SetBinding(TextBlock.TextProperty, b);
+
+
+        }
+
+        private void Button_StartEnd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Stir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Обработчик выбора количества попыток.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_NumberStirs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = (ComboBox)sender;
+            ComboBoxItem item = ( ComboBoxItem)cb.SelectedItem;
+            if(item.Content is not null)
+            {
+                string val = (string) item.Content;
+                Int32.TryParse(val, out numberAttempts);
+                
+            }        
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var rb = (RadioButton)sender;
+            var speedKey = (string) rb.Content;
+            if(speedKey != null)
+            {
+                speedVariants.TryGetValue(speedKey, out speed);
+                
+            }
+
         }
     }
 }
